47b126d01777a662f332fb8c74b25bc54577276c
Liudmyla <luda.skopenko80@gmail.com>
2024-03-18 19:18:19 +0300

    added try except

--- vlad.py
+++ vlad.py
@@ -1,24 +1,27 @@
-def calculate_profit(one:str)->int:
-    result = 0
-    one = int(one)
-    if one > 0:
-        if one <= 100_000:
-            result = one - one * 0.05
-            return result
-        elif one <= 1_000_000:
-            result = one - one * 0.08
-            return result
+def calculate_profit(one:str)->int|str:
+    try:
+        result = 0
+        one = int(one)
+        if one > 0:
+                if one <= 100_000:
+                    result = one - one * 0.05
+                    return result
+                elif one <= 1_000_000:
+                    result = one - one * 0.08
+                    return result
+                else:
+                    result = one - one * 0.15 
+                    return result
         else:
-            result = one - one * 0.15 
-            return result
-    else:
-        return "no profit"        
+                return "no profit"
+    except ValueError:
+        return "Invalid input: Please provide a valid integer."        
         
          
 
 
-print(calculate_profit("100_000_000")) 
-                             
+print(calculate_profit(" kykareky")) 
+#Оброботать ошибки                          
         
         
 
