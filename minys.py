af962b3cc1024e1e58c54aad4a77159a7f7ff1d0
Liudmyla <luda.skopenko80@gmail.com>
2024-03-19 23:03:16 +0300

    added minys

---
+++ minys.py
@@ -0,0 +1,29 @@
+# result = None
+# while True:
+#     try:
+#         a = input("Введіть число ")
+#         if a == "=":
+#             print(result)
+#             break
+#         if result == None:
+#             result =float(a)
+#         else:
+#             result -=float( a )
+#         print(f'Ваше число :{result} ')
+#     except ValueError:
+#         print(f"Введіть баранчики число ") 
+#         continue 
+import pprint  
+    
+age = 'Oleg 190, vlad 12000, liudmyla 35000, Alissa 12, Pusha 89, we 60000'
+inaf = age.split(',')
+
+
+
+print(inaf)
+
+
+
+
+
+
