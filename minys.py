745a53f906aa2b2c7ce77fa625b8398ebb9dc0b0
Liudmyla <luda.skopenko80@gmail.com>
2024-03-19 23:59:55 +0300

    added tabulate

--- minys.py
+++ minys.py
@@ -1,26 +1,39 @@
-# result = None
-# while True:
-#     try:
-#         a = input("Введіть число ")
-#         if a == "=":
-#             print(result)
-#             break
-#         if result == None:
-#             result =float(a)
-#         else:
-#             result -=float( a )
-#         print(f'Ваше число :{result} ')
-#     except ValueError:
-#         print(f"Введіть баранчики число ") 
-#         continue 
-import pprint  
+from pprint import pprint 
+
+
+from colorama import Fore 
+from tabulate import tabulate
+
+
     
 age = 'Oleg 190, vlad 12000, liudmyla 35000, Alissa 12, Pusha 89, we 60000'
-inaf = age.split(',')
+inaf = age.strip().split(',')
+dict_1 = {}
+for anabely in inaf:
+    grant = anabely.strip().split(" ")
+    dict_1[grant[0]] = grant[1]
 
 
 
-print(inaf)
+
+table_data = []
+for name, salary in dict_1.items():
+    name_and_selary = []
+    name_and_selary.append(name)
+    name_and_selary.append(salary)
+    table_data.append(name_and_selary)
+
+
+
+print(Fore.LIGHTBLUE_EX + tabulate(table_data, headers=['name','value'], tablefmt='grid'))
+
+    
+    
+
+
+
+
+
 
 
 
