4a35dae27e11ded22b78951e966280ccb41c2393
Liudmyla <luda.skopenko80@gmail.com>
2024-03-23 00:09:10 +0300

    added vlad

--- while.py
+++ while.py
@@ -6,6 +6,9 @@ import pyttsx3
 
 count = 0
 engine = pyttsx3.init()
+engine.setProperty('rate', 180)
+
+
 
 while True:
 
@@ -17,15 +20,15 @@ while True:
     
     elif user_input == 'oleg':
         print(Fore.LIGHTRED_EX + f'Вітаю Олеже як себе почуваєш?   ' )
-        engine.say('Вітаю Олеже як себе почуваєш?   ')
+        engine.say('   ')
         engine.runAndWait()
     elif user_input == 'vlad':
          print(Fore.LIGHTRED_EX + f'Вітаю, Владе як у тебе справи? як твої учні, достають тебе?  ')
-         engine.say('Вітаю, Владе як у тебе справи? як твої учні, достають тебе?  ')
+         engine.say('Здравствуй Владислав  как ты думаешь от чего кружится голова наверное от пайтона? ')
          engine.runAndWait()
     elif user_input == 'liuda':
         print(Fore.LIGHTRED_EX + f'Вітаю, ти добре вивчаєш пайтон?  ')
-        engine.say('Вітаю,Людмило ти добре вивчаєш пайтон?  ')
+        engine.say('Добрый день Людмила как ты себя чувствуешь и как твои успехи в питоне? ты хорошо контролируешь своих пацанов? ')
         engine.runAndWait()
     elif user_input == 'ok':
         print(Fore.BLACK + f'я бажаю тобі всего найкращого і щоб ти досягав перемоги над собою !!!  ')     
