e892a54ced17a28fb65c5a229df5896cd356cf55
Liudmyla <luda.skopenko80@gmail.com>
2024-03-23 00:32:59 +0300

    added practice

--- while.py
+++ while.py
@@ -20,18 +20,19 @@ while True:
     
     elif user_input == 'oleg':
         print(Fore.LIGHTRED_EX + f'Вітаю Олеже як себе почуваєш?   ' )
-        engine.say('   ')
+        engine.say('Олежка  добрый день , как ты  думаешь когда война закончится? сил уже нет никаких.   ')
         engine.runAndWait()
     elif user_input == 'vlad':
          print(Fore.LIGHTRED_EX + f'Вітаю, Владе як у тебе справи? як твої учні, достають тебе?  ')
-         engine.say('Здравствуй Владислав  как ты думаешь от чего кружится голова наверное от пайтона? ')
+         engine.say('Здравствуй Владислав  как ты думаешь от чего кружится голова.vlad наверное от пайтона? ')
          engine.runAndWait()
     elif user_input == 'liuda':
         print(Fore.LIGHTRED_EX + f'Вітаю, ти добре вивчаєш пайтон?  ')
         engine.say('Добрый день Людмила как ты себя чувствуешь и как твои успехи в питоне? ты хорошо контролируешь своих пацанов? ')
         engine.runAndWait()
     elif user_input == 'ok':
-        print(Fore.BLACK + f'я бажаю тобі всего найкращого і щоб ти досягав перемоги над собою !!!  ')     
-
+        print(Fore.BLACK + f' ')
+        engine.say('я хочу сказать вам всем уважаемые  мои люди что война закончится скоро и у нас всё будет хорошо. и это бутет так. поверьте мне')     
+        engine.runAndWait()
     else:
         print(Fore.LIGHTYELLOW_EX + f'Введіть текст : {user_input}')
