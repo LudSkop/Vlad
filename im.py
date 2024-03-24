891b35c4bc4d84780eec5b6f46fafdf9cc3e4651
Liudmyla <luda.skopenko80@gmail.com>
2024-03-24 23:10:35 +0300

    added polindrom

--- im.py
+++ im.py
@@ -1,12 +1,13 @@
 # чи являється 3-х значне число поліндромом.
+
 try:
     namber = int(input('integen:  '))
     nam_1 = namber // 100
     nam_2 = namber % 10
 
     if nam_1 == nam_2:
-        print('yes')
+            print('yes')                
     else:
-        print('no')
+            print('no')
 except ValueError:
     print('invalid input')
