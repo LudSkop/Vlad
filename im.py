f57096b7947368b3a62bfb78526ee1e1328ad9b6
Liudmyla <luda.skopenko80@gmail.com>
2024-03-26 01:58:12 +0300

    added im.py

--- im.py
+++ im.py
@@ -1,17 +1,9 @@
-#перевірити чи в 3-х значному всі числа різні. І чи є тільки 2 однакових числа.
- 
-result = input('Enter 3 digit namber:  ')
-if len(result) != 3:
-    print('Please provide a valid integen:  ')
-else:
-    res_1 = int(result[0])
-    res_2 = int(result[1])
-    res_3 = int(result[2])
-
-    if res_1 != res_3 and res_2 != res_3 and res_2!= res_1:
-        print('Nambers are diffrent ')
-    elif (res_1 == res_3 or res_2 == res_3 or res_1 == res_2) and not ( res_1 == res_2 or res_1 == res_3):
-        print('2 same  namber')
+a = 20
+b = 30
+sume = a
+a = b
+b = sume
+print(f'a: {a} b: {b}')
     
          
 
