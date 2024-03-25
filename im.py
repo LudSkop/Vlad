200331cda440a467513eaec697b904958f1d5073
Liudmyla <luda.skopenko80@gmail.com>
2024-03-25 19:58:53 +0300

    added im py

--- im.py
+++ im.py
@@ -1,13 +1,27 @@
-# чи являється 3-х значне число поліндромом.
+#перевірити чи в 3-х значному всі числа різні. І чи є тільки 2 однакових числа.
+ 
+result = input('Enter 3 digit namber:  ')
+if len(result) != 3:
+    print('Please provide a valid integen:  ')
+else:
+    res_1 = int(result[0])
+    res_2 = int(result[1])
+    res_3 = int(result[2])
 
-try:
-    namber = int(input('integen:  '))
-    nam_1 = namber // 100
-    nam_2 = namber % 10
+    if res_1 != res_3 and res_2 != res_3 and res_2!= res_1:
+        print('Nambers are diffrent ')
+    elif (res_1 == res_3 or res_2 == res_3 or res_1 == res_2) and not ( res_1 == res_2 or res_1 == res_3):
+        print('2 same  namber')
+    
+         
 
-    if nam_1 == nam_2:
-            print('yes')                
-    else:
-            print('no')
-except ValueError:
-    print('invalid input')
+
+
+
+
+
+
+
+
+
+
